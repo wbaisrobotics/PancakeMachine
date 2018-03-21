9f007ec02c3a7ff4d86d3362ca7bd80f6d55227b
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2018-03-21 19:23:41 +0200

    Initial setup for the solenoids and the motors

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Solenoid.hpp"
 
+// Initialize the dispenser on pin 13
+Solenoid dispenserStopper (13);
+
+// Initialize the arm extendor on pin 12
+Solenoid armExtendor (12);
+
+// Initialize the pancake flipper on pin 11
+Solenoid pancakeFlipper (11);
+
+
+// Initialize the mixing motor on pin 10
+PWMMotorController mixingMotor (10);
+
+// Initialize the other motor on pin 9
+PWMMotorController otherMotor (9);
+
+
+void setup() {
 }
 
+// Move the solenoid to forward position, wait 5 seconds, move the solenoid to reverse position, wait 5 seconds
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+ 
+  
 }
