3d912bd99c11d2ad6cf89cfa442fe568f763cf8b
oleitersdorf <31508502+oleitersdorf@users.noreply.github.com>
2018-04-12 15:02:48 +0300

    Tested with Relays

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,29 +1,72 @@
 #include "Solenoid.hpp"
-#include "PWMMotorController.hpp"
 
-// Initialize the dispenser on pin 13
-Solenoid dispenserStopper (13);
+Solenoid dispenser (36);
 
-// Initialize the arm extendor on pin 12
-Solenoid armExtendor (12);
+Solenoid extender (34);
 
-// Initialize the pancake flipper on pin 11
-Solenoid pancakeFlipper (11);
-
-
-// Initialize the mixing motor on pin 10
-PWMMotorController mixingMotor (10);
-
-// Initialize the other motor on pin 9
-PWMMotorController otherMotor (9);
-
+Solenoid rotator (32);
 
 void setup() {
 }
 
 // Move the solenoid to forward position, wait 5 seconds, move the solenoid to reverse position, wait 5 seconds
 void loop() {
+
+  rotateOut();
+  addBatter();
+  delay (10000);
+  rotateIn();
+  extendOut();
+  delay(2000);
+  extendIn();
+  rotateOut();
   
- 
+//
+//dispenser.forward();
+//rotator.forward();
+//extender.forward();
+
+  //delay(10000);
   
 }
+
+// Adds batter onto the plate
+// Should be called after rotateOut()
+void addBatter (){
+  dispenser.forward();
+  delay(5000);
+  dispenser.reverse();
+}
+
+// Rotates the arm out
+void rotateOut(){
+  if (rotator.isReverse()){
+    rotator.forward();
+    delay(1000);
+  }
+}
+
+// Rotates the arm in
+void rotateIn(){
+  if (rotator.isForward()){
+    rotator.reverse();
+    delay(1000);
+  }
+}
+
+// Extends the arm out
+void extendOut(){
+  if (extender.isReverse()){
+    extender.forward();
+    delay (1000);
+  }
+}
+
+// Extends the arm in
+void extendIn(){
+  if (extender.isForward()){
+    extender.reverse();
+    delay (1000);
+  }
+}
+
